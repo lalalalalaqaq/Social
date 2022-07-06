340fb7fe679a6e8fa3f5ba3672c660c3233115c7
orange.Zhang <orange.zhang@atomintl.com>
2022-07-07 00:59:37 +0800

    init backend

---
+++ LoginProject/src/main/java/com/edu/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.edu.project.mapper;
+
+import com.edu.project.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * UserMapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
